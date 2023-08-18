3fe9d85eebd7257f97655009d5e11a3244b37ef4
SpeedCrash100 <SpeedCrash100@yandex.ru>
2023-08-18 09:27:39 +0300

    Use deque in Median filter

--- src/filter/median.rs
+++ src/filter/median.rs
@@ -1,28 +1,26 @@
 use super::Filter;
-use heapless::Vec;
+use heapless::{Deque, Vec};
 
 /// Median filter with custom size
 /// N should be a odd number
 pub struct Median<const N: usize> {
-    buffer: Vec<f32, N>,
+    buffer: Deque<f32, N>,
 }
 
 impl<const N: usize> Default for Median<N> {
     fn default() -> Self {
-        let mut buffer = Vec::new();
-        buffer
-            .resize(N, 0.0)
-            .expect("resize bug: resizing to N elements with N-sized buffer failed");
+        let mut buffer = Deque::new();
+        while !buffer.is_full() {
+            buffer.push_back(0.0).ok();
+        }
         Self { buffer }
     }
 }
 
 impl<const N: usize> Filter for Median<N> {
     fn insert(&mut self, value: f32) {
-        self.buffer.remove(0);
-        self.buffer.push(value).expect(
-            "heapless::Vec bug: after removing 1 element and inserting new one push must succeed",
-        );
+        self.buffer.pop_front();
+        self.buffer.push_back(value).ok();
     }
 
     fn filtered(&self) -> f32 {
